b1838b2e6957a9dd90a477580e114eee2e7e29bd
Tristan <94827589+chiwu7@users.noreply.github.com>
2023-01-15 21:17:04 +0000

    ListeTableau fini // manque commentaire et javadoc

--- fr/rodez3il/a2022/mrmatt/solveur/structures/ListeChainee.java
+++ fr/rodez3il/a2022/mrmatt/solveur/structures/ListeChainee.java
@@ -2,4 +2,18 @@ package fr.rodez3il.a2022.mrmatt.solveur.structures;
 
 public class ListeChainee<T> implements Liste<T> {
 
+  class Maillon {
+    private T donnee;
+    private Maillon suivant;
+  }
+
+  public Maillon tete;
+  private int taille;
+
+  public ListeChainee() {
+    this.tete = null;
+    this.taille = 0;
+  }
+
+  
 }
