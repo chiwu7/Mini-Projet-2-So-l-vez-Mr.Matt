b21b1aa136bb89a7fec4d98e5c5d4b31d4c8b7e8
Tristan <94827589+chiwu7@users.noreply.github.com>
2023-02-02 23:36:51 +0000

    Solveur // JavaDoc + commentaire

--- fr/rodez3il/a2022/mrmatt/solveur/Solveur.java
+++ fr/rodez3il/a2022/mrmatt/solveur/Solveur.java
@@ -5,6 +5,11 @@ import fr.rodez3il.a2022.mrmatt.sources.Niveau;
 
 public class Solveur {
 
+  /**
+   * Trouve une solution pour un niveau donnée
+   * @param le niveau pour lequel on cherche une solution
+   * @return la chaine de caractères représentant la solution du niveau
+  */
 	public static String trouverSolution(Niveau niveau) {
 
   DictionnaireChaine<String, Noeud> configConnu = new DictionnaireChaine<>();
@@ -18,21 +23,27 @@ public class Solveur {
 
   String solution = null;
 
-  while (!aTraiter.estVide() && solution ==null){
+  //tant que la liste est pas vide et qu'on a pas trouvé la solution, on boucle
+  while (!aTraiter.estVide() && solution == null){
 
+    //on récupère la valeur du premier noeud de la liste à traiter
     Noeud n = aTraiter.enlever(0);
+    //on l'ajoute à la liste des noeud traités
     traite.ajouter(n);
-  
+
+    //on calcule les fils du noeud que l'on traite
     solution = n.calculerFils();
-    
-    if(solution==null) {
+
+    //Si on a pas de solution
+    if(solution == null) {
+      //on recupère la liste des fils du noeud qu'on traite
       ListeTableau<Noeud> tabFils = n.getFils();
+      //on boucle sur la liste pour récupèrer les noeuds individuellement
       for(int i = 0; i < tabFils.taille(); i++) {
         Noeud fils = tabFils.element(i);
-        if(!aTraiter.contient(fils) && !traite.contient(fils) && !fils.estVisite()){
-
+        //si ils sont dans aucune liste et qu'ils n'ont pas été visité on les ajoute à la liste aTraiter
+        if(!aTraiter.contient(fils) && !traite.contient(fils) && !fils.estVisite())
           aTraiter.ajouter(fils);
-        }
       }
     } 
 
