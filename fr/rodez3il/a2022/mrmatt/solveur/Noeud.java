6286f65716f037966ebfc019cbdb0274d535a9a0
Tristan <94827589+chiwu7@users.noreply.github.com>
2023-02-02 23:27:34 +0000

    Noeud // Refactoring + Javadoc

--- fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
+++ fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
@@ -5,12 +5,30 @@ import fr.rodez3il.a2022.mrmatt.solveur.structures.*;
 
 public class Noeud {
 
+  //dictionnaire qui va stocker les différentes configurations connues
   private DictionnaireChaine<String, Noeud> config;
+  //référence vers l'état du niveau actuel
   private Niveau etat;
+  //fils du noeud
   private ListeTableau<Noeud> fils;
+  //représente la succession de commandes
   private String commandes;
+  //permet de connaitre l'état de visite du noeud.
   private boolean visite;
+  //chaine de caractère de la solution du niveau
+  private String solution = null;
+  //chaine de caractère représentant la succession des commandes
+  private String suiteCommandes;
+  //représente l'état du niveau sous forme de caractères
+  String valChaine;
 
+  /**
+   * Constructeur pour la classe Noeud.
+   * @param config Le dictionnaire associant une commande à un Noeud.
+   * @param etat Le niveau actuel.
+   * @param commandes La succession de commandes associées à ce Noeud.
+   * @return un nouveau noeud
+   */
   public Noeud(DictionnaireChaine<String, Noeud> config, Niveau etat, String commandes) {
     this.config = config;
     this.etat = etat;
@@ -19,24 +37,35 @@ public class Noeud {
     this.visite = false;
   }
 
+  /**
+   * Indique si le noeud à déjà été visité.
+   * @return vrai ssi le noeud à été visité
+   */  
   public boolean estVisite() {
     return this.visite;
   }
 
+  /**
+   * Retourne la liste des fils du noeud actuel
+   * @return la liste du noeud actuel
+   */  
   public ListeTableau<Noeud> getFils() {
     return this.fils;
   }
 
+  /**
+   * Indique si le noeud à déjà été visité.
+   * @return vrai ssi le noeud à été visité
+    */  
   public void ajouterFils(Noeud fils) {
     if (fils != null) {
       this.fils.ajouter(fils);
     }
   }
 
-  public Niveau getEtat() {
-    return this.etat;
-  }
-
+  /**
+   * Redéfinition de la méthode equals
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
@@ -69,21 +98,25 @@ public class Noeud {
     return true;
   }
 
+  /**
+   * Calcule les fils du noeud actuel
+   * @return retourne la solution du niveau ssi le niveau est gagnant. Sinon retourne null
+  */
+  
   public String calculerFils() {
-    String victoire = null;
-    String solution = null;
     this.visite = true;
-      //on transforme l'état du niveau en un String
+    //on le noeud actuel avec l'état du niveau dans le dictionnaire de configurations
     config.inserer(etat.valeurChaine(), this);
-
+      //on récupère les valeurs de l'énumérations
       Commande[] commande = Commande.values();
-
+      //On boucle 4 fois pour passer dans toutes les directions
       for (int i = 0; i < 4; i++) {
-        //on regarde si le joueur peut se déplacer et si l
+        //on regarde si le joueur peut se déplacer et si la partie de jeu (niveau) est toujours en cours
         if (etat.deplacementPossible(commande[i]) && etat.enCours()) {
 
-          solution = this.commandes + " " + commande[i];
-          
+          suiteCommandes = this.commandes + " " + commande[i];
+
+          //on copie l'etat du niveau
           Niveau nouvelEtat = etat.copier();
 
           //on déplace le joueur
@@ -93,25 +126,30 @@ public class Noeud {
           nouvelEtat.calcule();
 
           boolean gagnant = nouvelEtat.estGagnant();
-          
+
+          //Si le niveau est gagant, on renvoie la chaine de caractère contenant la solution
           if (gagnant) {
-            victoire = solution;
+            solution = suiteCommandes;
           } else {
             Noeud filsNoeud;
-            
-            if (config.contient(nouvelEtat.valeurChaine())) {
-              
-              filsNoeud = config.valeur(nouvelEtat.valeurChaine());
+            //on calcule la valeurChaine du niveau
+            valChaine = nouvelEtat.valeurChaine();
+            //on regarde si elle est dans le dictionnaire des configurations
+            if (config.contient(valChaine)) {
+              //si elle est dans le dictionnaire, on récupère sa valeur
+              filsNoeud = config.valeur(valChaine);
             } else {
-              filsNoeud = new Noeud(this.config, nouvelEtat, solution);
-              this.config.inserer(nouvelEtat.valeurChaine(), filsNoeud);
+              //si elle est pas dans le dictionnaire on l'ajoute
+              filsNoeud = new Noeud(this.config, nouvelEtat, suiteCommandes);
+              this.config.inserer(valChaine, filsNoeud);
             }
+            //on ajoute le noeud à la liste
             this.fils.ajouter(filsNoeud);
           }
         }
       }
 
-    return victoire;
+    return solution;
 }
 
 }
